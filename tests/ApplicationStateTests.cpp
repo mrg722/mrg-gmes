fa3afaeb8e68c13cfe8aa166f704005528cc1f41
mrg722 <mar.reyesg@duocuc.cl>
2026-09-13 00:42:03 -0300

    test: fix illegal physical newline in string literal

--- tests/ApplicationStateTests.cpp
+++ tests/ApplicationStateTests.cpp
@@ -8,14 +8,12 @@ int main() {
     using district_fury::core::shouldContinue;
 
     if (!shouldContinue(ApplicationState::Running)) {
-        std::cerr << "A running application must continue.
-";
+        std::cerr << "A running application must continue.\n";
         return EXIT_FAILURE;
     }
 
     if (shouldContinue(ApplicationState::ExitRequested)) {
-        std::cerr << "An exit request must stop the application.
-";
+        std::cerr << "An exit request must stop the application.\n";
         return EXIT_FAILURE;
     }
 
